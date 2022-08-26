4e4d64fb5923157c2aade2a2870a4ca54f965102
Yana Kushnerova <yanakushneriova@gmail.com>
2022-08-26 18:31:20 +0300

    sorting and filtering on front

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
--- frontend/src/components/ApartmentList.js
+++ frontend/src/components/ApartmentList.js
@@ -4,19 +4,64 @@ import ApartmentListItem from "./ApartmentListItem"
 
 const ApartmentList = () => {
     const [apartments, setApartments] = useState([])
+    const [sortPrice, setSortPrice] = useState('none')
+    const [filterRooms, setFilterRooms] = useState('')
+
+    const onSortPriceChange = (e) => {
+        if (e.target.value === 'asc') {
+            setSortPrice('asc')
+        } else if (e.target.value === 'desc') {
+            setSortPrice('desc')
+        } else {
+            setSortPrice('none')
+        }
+    }
+
+    const onFilterRoomsChange = (e) => {
+        const rooms = e.target.value
+        
+        if (rooms.match(/^\d{0,}$/)) {
+            setFilterRooms(rooms)
+        }
+    }
 
     const loadApartments = (url = process.env.REACT_APP_API_URL + '/apartments') => {
-        axios.get(url).then(response => {
+        let query = ''
+
+        if (filterRooms) {
+            query += '?rooms=' + parseInt(filterRooms)
+        } 
+        
+        if (sortPrice !== 'none') {
+            !query ? query += '?price=' + sortPrice : query += '&price=' + sortPrice
+        }
+
+        axios.get(url + query).then(response => {
             setApartments(response.data)
         }).catch(error => {
             console.log(error)
         })
     }
 
+    const onApplyFilters = (e) => {
+        e.preventDefault()
+        loadApartments()
+    }
+
     useEffect(loadApartments, [])
 
     return (
         <div>
+            <p>sort by price</p>
+            <select value={sortPrice} onChange={onSortPriceChange}>
+                <option value='none'>none</option>
+                <option value='asc'>from lowest to highest</option>
+                <option value='desc'>from highest to lowest</option>
+            </select>
+            
+            <input type='text' value={filterRooms} onChange={onFilterRoomsChange} placeholder="number of rooms"></input>
+            <button onClick={onApplyFilters}>search</button>
+
             <p>Available apartments: {apartments.length}</p>
             { apartments ? (
                 apartments.map((apartment) => {
